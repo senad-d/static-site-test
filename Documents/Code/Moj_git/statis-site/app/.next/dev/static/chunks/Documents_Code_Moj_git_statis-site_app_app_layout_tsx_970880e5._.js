3ddf0f2b1e8cd21f81c831f4b10af3f6865e8e77
Senad  Dizdarević <112484166+senad-d@users.noreply.github.com>
2025-12-29 18:25:11 +0100

    fix

--- Documents/Code/Moj_git/statis-site/app/.next/dev/static/chunks/Documents_Code_Moj_git_statis-site_app_app_layout_tsx_970880e5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9d1799a2._.css",
-  "static/chunks/Documents_Code_Moj_git_statis-site_app_f91fb1c6._.js"
-],
-    source: "dynamic"
-});
--- Documents/Code/Moj_git/statis-site/app/.next/dev/static/chunks/Documents_Code_Moj_git_statis-site_app_app_page_tsx_ff9098a4._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/Documents_Code_Moj_git_statis-site_app_6e13a7de._.js",
-  "static/chunks/d0532_12867499._.js"
-],
-    source: "dynamic"
-});
