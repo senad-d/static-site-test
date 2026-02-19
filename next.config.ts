f099d4993dd3575c17d9e5bf33e1644e753e0185
Senad  Dizdarević <112484166+senad-d@users.noreply.github.com>
2026-02-19 09:28:15 +0100

    build(config): adjust base path env for github actions

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const isGitHubPages = process.env.GITHUB_PAGES === "true";
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
 const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
 const isUserOrOrgPage = !!repo && repo.endsWith(".github.io");
 
-const basePath = isGitHubPages && repo && !isUserOrOrgPage ? `/${repo}` : "";
+const basePath = isGitHubActions && repo && !isUserOrOrgPage ? `/${repo}` : "";
 
 const nextConfig: NextConfig = {
   output: "export",
